b3dea9208338ee06b7b71054e1e3bc984183a4d2
houssam440 <houssam.baaloul78@gmail.com>
2020-07-02 16:21:00 +0100

    Updates

---
+++ precache-manifest.e0747161eb1d58c77794fbe93f426e99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55879df3a0ceb3d7b6db90fb99db5b0f",
+    "url": "/RoboFriends06/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/RoboFriends06/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d7af0891c79b46c40411",
+    "url": "/RoboFriends06/static/css/main.0a671d1b.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/RoboFriends06/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends06/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7af0891c79b46c40411",
+    "url": "/RoboFriends06/static/js/main.e3170b3a.chunk.js"
+  },
+  {
+    "revision": "e422c14d8e3590a0e38e",
+    "url": "/RoboFriends06/static/js/runtime-main.940c404a.js"
+  }
+]);
